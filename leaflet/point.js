52f505b446736fea4350b191608880588c78bb1f
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2026-06-24 03:07:52 +0530

    Add files via upload

---
+++ leaflet/point.js
@@ -0,0 +1,44 @@
+var point = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Navigate": "<a href=\"https://maps.google.com/maps?z=18&q=13.1011081,77.5145891\" target=\"_blank\">go</a>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.5145891,
+          13.1011081
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Navigate": "<a href=\"https://maps.google.com/maps?z=18&q=13.1048071,77.5180349\" target=\"_blank\">go</a>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.5180349,
+          13.1048071
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Navigate": "<a href=\"https://maps.google.com/maps?z=18&q=\n13.1011481,77.5193393\" target=\"_blank\">go</a>\n          "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.5193393,
+          13.1011481
+        ]
+      }
+    }
+  ]
+}
